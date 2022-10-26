9a9dbfc2840750536a53bc2c4ffa60ae86c4f64f
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-26 16:36:57 +0200

    Adds js const

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const userFirstName = prompt("Insert your first name");
+const userLastName = prompt("Insert your last name");
+const userColor = prompt("Insert your favourite color");
+
+/* console.log("First name:", userFirstName);
+console.log("Typology:", typeof userFirstName);
+
+console.log("Last name:", userLastName);
+console.log("Typology:", typeof userLastName);
+
+console.log("Favourite color:", userColor);
+console.log("Typology:", typeof userColor);
+ */
